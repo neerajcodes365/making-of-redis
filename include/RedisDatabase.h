c43993f5e3acd53bfd8ae062051aa186cf2295c6
Neeraj J <neeraj2gokul4@gmail.com>
2026-06-28 10:51:18 +0000

    stage 6 completed, command_handler_ expire and rename inner if nte else baki ind

--- include/RedisDatabase.h
+++ include/RedisDatabase.h
@@ -5,11 +5,31 @@
 #include<mutex>
 #include<unordered_map>
 #include<vector>
+#include<chrono>
+
 class RedisDatabase {
 
     public:
     //singleton instance
     static RedisDatabase& getInstance();
+    //common commands
+    bool flushAll();
+
+
+    // key/value operations
+    void set(const std::string&key,const std::string&value);
+    bool get(const std::string&key ,const std::string&value);
+    std::vector<std::string>keys();
+
+
+    //Types
+    std::string type(const std::string&key);
+    bool del(const std::string& key);
+    bool expire(const std::string&key,const std::string& seconds);
+    bool rename(const std::string& oldkey,const std::string& newkey);
+    
+    
+
 
     //persistance: dump,load the database from a file
      bool dump(const std::string& filename);
@@ -27,6 +47,7 @@ class RedisDatabase {
     std::unordered_map<std::string,std::vector<std::string>>list_store;
     std::unordered_map<std::string,std::unordered_map<std::string,std::string>>hash_store;
 
+    std::unordered_map<std::string,std::chrono::steady_clock::time_point>expiry_map;//for expire fun.
 };
 
 #endif
--- src/RedisCommandHandler.cpp
+++ src/RedisCommandHandler.cpp
@@ -110,25 +110,100 @@ Now:s == "Hello World"
 
 
     //connect to database
-    // RedisDatabase& db=RedisDatabase::getInstance();//will build
+    RedisDatabase& db=RedisDatabase::getInstance();
 
-//debugging
-// std::cout << "Command is = [" << cmd << "]\n";
-    //check commands
-    if (cmd == "PING")
-{
-    response << "+PONG\r\n";
-}
+    //check common commands
+    if (cmd == "PING"){
+        response << "+PONG\r\n";
+    } 
     else if(cmd=="ECHO"){
-        // ....
-        // response<<"echo is done\r\n";
+        if(tokens.size()<2){
+            response<<"Error: ECHO needs two parameters";
+        }
+        else{
+            response<<"+"<<tokens[1]<<"\r\n";
+        }
     }
+    else if(cmd=="FLUSHALL"){
+        db.flushAll();
+        response<<"+OK\r\n";
+    }
+    // keyvalue operations
+    else if(cmd=="SET"){
+            if(tokens.size()<3){
+                response<<"Error : SET needs 3 parameters";
+            }
+            else{
+                db.set(tokens[1],tokens[2]);
+                response<<"+OK\r\n";
+            }
+    }
+    else if(cmd=="GET"){
+            if(tokens.size()<2){
+                response<<"Error : GET requires key\r\n";
+            }
+            else{
+                std::string value;
+                if(db.get(tokens[1],value)){
+                    response<<"$"<<value.size()<<"\r\n"<<value<<"\r\n";
+                }
+                else{
+                    response<<"$-1\r\n";
+                }
+            }
+    }
+    else if( cmd=="KEYS"){
+        std::vector<std::string> allkeys=db.keys();
+        response<<"*"<<allkeys.size()<<"\r\n";
+        for(const auto&key:allkeys){
+            response<<"$"<<key.size()<<"\r\n"<<key<<"\r\n";
+        }
+    }
+    else if(cmd=="TYPE"){
+          if(tokens.size()<2){
+                response<<"Error : TYPE requires key\r\n";
+            }
+            else{
+                response<<"+"<<db.type(tokens[1])<<"\r\n";
+            }
+    }
+    else if(cmd=="DEL"|| cmd=="UNLINK"){
+        if(tokens.size()<2){
+            response<<"-Error :"<<cmd<<"requires key \r\n";
+        }
+        else{
+            bool res=db.del(tokens[1]);
+            response<<":"<<(res?1:0)<<"\r\n";
+        }
+    }
+    else if(cmd=="EXPIRE"){
+        if(tokens.size()<3){
+            response<<"-Error: EXPIRE requires key and time in seconds\r\n";
+        }
+        else{
+            if(db.expire(tokens[1],tokens[2])){
+                response<<"+OK\r\n";
+            }//will complete in future
+            else{
+                // response<<will do in next stage 
+            }
+        }
+    }
+    else if(cmd=="RENAME"){
+         if(tokens.size()<3){
+            response<<"-Error: RENAME requires oldkey and newkey name \r\n";
+        }else{
+            if(db.rename(tokens[1],tokens[2])){
+                response<<"+OK\r\n";
+            }
+        }
+    }
+
+
+    //list operations
+    //hash operations
     else{
         response<<"-Error : Unknown Command \r\n";
     }
-    // keyvalue operations
-    //list operations
-    //hash operations
-
     return response.str();
 }
